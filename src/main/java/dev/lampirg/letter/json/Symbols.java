d33bd20f61e9e134c8a76624501e9d16cc6ba2d4
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-09-28 13:34:27 +0300

    Update Symbols record
    Remove unused constructor.

--- src/main/java/dev/lampirg/letter/json/Symbols.java
+++ src/main/java/dev/lampirg/letter/json/Symbols.java
@@ -4,11 +4,4 @@ import java.util.List;
 import java.util.Map;
 
 public record Symbols(List<Symbol> symbols) {
-    public Symbols(Map<Character, Integer> map) {
-        this(
-                map.entrySet().stream()
-                .map(entry -> new Symbol(entry.getKey(), entry.getValue()))
-                .toList()
-        );
-    }
 }
